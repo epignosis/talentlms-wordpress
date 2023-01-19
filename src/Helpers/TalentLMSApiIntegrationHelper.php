d196ca0dfba246ff54c8eb799b4cd35c4dd84eef
cpsychogyios <95210555+cpsychogyios@users.noreply.github.com>
2023-01-19 14:23:33 +0200

    Introduce new helper trait for setting api keys and reformat

---
+++ src/Helpers/TalentLMSApiIntegrationHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace TalentlmsIntegration\Helpers;
+
+use Exception;
+use TalentLMS;
+use TalentLMS_ApiError;
+
+trait TalentLMSApiIntegrationHelper{
+
+	/**
+	 * @throws TalentLMS_ApiError
+	 */
+	public function enableTalentLMSLib(): void{
+		if(empty(get_option('tlms-domain')) || empty(get_option('tlms-apikey'))){
+			throw new TalentLMS_ApiError(__('You need to specify a TalentLMS domain and a TalentLMS API key.', 'talentlms'));
+		}
+
+		try{
+			TalentLMS::setDomain(get_option('tlms-domain'));
+			TalentLMS::setApiKey(get_option('tlms-apikey'));
+		}
+		catch(Exception $e){
+			if($e instanceof TalentLMS_ApiError){
+				tlms_logError($e->getMessage());
+			}
+		}
+	}
+}
--- src/Pages/Admin.php
+++ src/Pages/Admin.php
@@ -5,43 +5,52 @@
 
 namespace TalentlmsIntegration\Pages;
 
+use TalentLMS_ApiError;
+use TalentlmsIntegration\Helpers\TalentLMSApiIntegrationHelper;
 use TalentlmsIntegration\Services\PluginService;
 use TalentlmsIntegration\Utils;
 
 class Admin implements PluginService{
 
+	use TalentLMSApiIntegrationHelper;
+
+	/**
+	 * @throws TalentLMS_ApiError
+	 */
 	public function register(): void{
 		add_action('admin_menu', array($this, 'tlms_registerAdministrationPages'));
+		$this->enableTalentLMSLib();
 	}
 
-	public function tlms_registerAdministrationPages() {
+	public function tlms_registerAdministrationPages(){
 		add_menu_page(__('TalentLMS', 'talentlms'), __('TalentLMS', 'talentlms'), 'manage_options', 'talentlms', array($this, 'tlms_adminPanel'));
 		add_submenu_page('talentlms', __('Dashboard', 'talentlms'), __('Dashboard', 'talentlms'), 'manage_options', 'talentlms', array($this, 'tlms_adminPanel'));
 		add_submenu_page('talentlms', __('Setup', 'talentlms'), __('Setup', 'talentlms'), 'manage_options', 'talentlms-setup', array($this, 'tlms_setupPage'));
-		add_submenu_page('talentlms', __('Integrations', 'talentlms'), __('Integrations', 'talentlms'), 'manage_options', 'talentlms-integrations', array($this, 'tlms_integrationsPage'));
+		add_submenu_page('talentlms', __('Integrations', 'talentlms'), __('Integrations', 'talentlms'), 'manage_options', 'talentlms-integrations', array($this,
+																																						  'tlms_integrationsPage'));
 	}
 
-	function tlms_adminPanel() {
-		require_once TLMS_BASEPATH . '/templates/dashboard.php';
+	function tlms_adminPanel(){
+		require_once TLMS_BASEPATH.'/templates/dashboard.php';
 	}
 
-	public function tlms_setupPage () {
+	public function tlms_setupPage(){
 
 		$action_status = $action_message = $enroll_user_validation = $automatically_complete_orders = $api_validation = $domain_validation = '';
-		if (isset($_POST['action']) && $_POST['action'] == "tlms-setup") {
+		if(isset($_POST['action']) && $_POST['action'] == "tlms-setup"){
 
-			if ($_POST['tlms-domain'] && $_POST['tlms-apikey'] && $_POST['tlms-enroll-user-to-courses']) {
+			if($_POST['tlms-domain'] && $_POST['tlms-apikey'] && $_POST['tlms-enroll-user-to-courses']){
 
 				// we accept the domain only, without the protocol
-				if (strtolower(substr($_POST['tlms-domain'], 0, 4 )) === "http"){
+				if(strtolower(substr($_POST['tlms-domain'], 0, 4)) === "http"){
 					$action_status = "error";
 					$domain_validation = 'form-invalid';
-					$action_message = __('Invalid TalentLMS Domain', 'talentlms') . "<br />";
+					$action_message = __('Invalid TalentLMS Domain', 'talentlms')."<br />";
 				}
-				else if (strlen($_POST['tlms-apikey']) != 30){ // TalentLMS API key is exactly 30 characters
+				else if(strlen($_POST['tlms-apikey']) != 30){ // TalentLMS API key is exactly 30 characters
 					$action_status = "error";
 					$api_validation = 'form-invalid';
-					$action_message = __('Invalid TalentLMS API key', 'talentlms') . "<br />";
+					$action_message = __('Invalid TalentLMS API key', 'talentlms')."<br />";
 				}
 				else{
 					update_option('tlms-domain', $_POST['tlms-domain']);
@@ -53,36 +62,36 @@ class Admin implements PluginService{
 					$action_status = "updated";
 					$action_message = __('Details edited successfully', 'talentlms');
 				}
-
-			} else {
+			}
+			else{
 				$action_status = "error";
 
-				if (!$_POST['tlms-domain']) {
+				if(!$_POST['tlms-domain']){
 					$domain_validation = 'form-invalid';
-					$action_message = __('TalentLMS Domain required', 'talentlms') . "<br />";
+					$action_message = __('TalentLMS Domain required', 'talentlms')."<br />";
 					update_option('tlms-domain', '');
 				}
 
-				if (!$_POST['tlms-apikey']) {
+				if(!$_POST['tlms-apikey']){
 					$api_validation = 'form-invalid';
-					$action_message .= __('TalentLMS API key required', 'talentlms') . "<br />";
+					$action_message .= __('TalentLMS API key required', 'talentlms')."<br />";
 					update_option('tlms-apikey', '');
 				}
 			}
 		}
 
-		require_once TLMS_BASEPATH . '/templates/setup.php';
+		require_once TLMS_BASEPATH.'/templates/setup.php';
 	}
 
-	public function tlms_integrationsPage () {
+	public function tlms_integrationsPage(){
 
 		$courses = Utils::tlms_selectCourses();
 
-		if(isset($_POST['tlms_products']) && $_POST['tlms_products']) {
+		if(isset($_POST['tlms_products']) && $_POST['tlms_products']){
 			Utils::tlms_addProductCategories();
 
-			foreach ($_POST['tlms_products'] as $course_id) {
-				if(! Utils::tlms_productExists($course_id)) {
+			foreach($_POST['tlms_products'] as $course_id){
+				if(!Utils::tlms_productExists($course_id)){
 					Utils::tlms_addProduct($course_id, $courses);
 				}
 			}
@@ -92,10 +101,10 @@ class Admin implements PluginService{
 		}
 
 		if(isset($_POST['action']) && $_POST['action'] == 'tlms-fetch-courses'){//refresh courses
-			Utils::tlms_getCourses(true );
+			Utils::tlms_getCourses(true);
 			wp_redirect(admin_url('admin.php?page=talentlms-integrations'));
 		}
 
-		require_once TLMS_BASEPATH . '/templates/integrations.php';
+		require_once TLMS_BASEPATH.'/templates/integrations.php';
 	}
 }
