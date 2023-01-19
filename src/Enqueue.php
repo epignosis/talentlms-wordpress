6a201266d4f97ab5146b18fcb7ce25b1b5c38783
cpsychogyios <95210555+cpsychogyios@users.noreply.github.com>
2023-01-19 17:47:07 +0200

    Remove slashes, since already exist from TLMS_BASEURL

--- src/Enqueue.php
+++ src/Enqueue.php
@@ -15,10 +15,10 @@ class Enqueue implements PluginService{
 	}
 
 	private function tlms_commonLibs(){
-		wp_register_style('tlms-widget', TLMS_BASEURL . '/assets/css/talentlms-widget.css', false, TLMS_VERSION);
+		wp_register_style('tlms-widget', TLMS_BASEURL . 'assets/css/talentlms-widget.css', false, TLMS_VERSION);
 
-		wp_register_script('bootstrap-js', TLMS_BASEURL . '/assets/js/bootstrap.min.js', ['jquery'], false, true);
-		wp_register_script('tlms-font-awesome', TLMS_BASEURL . '/assets/js/font-awesome.min.js', false, TLMS_VERSION);
+		wp_register_script('bootstrap-js', TLMS_BASEURL . 'assets/js/bootstrap.min.js', ['jquery'], false, true);
+		wp_register_script('tlms-font-awesome', TLMS_BASEURL . 'assets/js/font-awesome.min.js', false, TLMS_VERSION);
 
 		wp_enqueue_style('tlms-widget');
 		wp_enqueue_script('bootstrap-js');
@@ -28,10 +28,10 @@ class Enqueue implements PluginService{
 		$this->tlms_commonLibs();
 
 		//Register styles
-		wp_register_style('tlms-admin', TLMS_BASEURL . '/assets/css/tlms-admin.css', false, TLMS_VERSION);
+		wp_register_style('tlms-admin', TLMS_BASEURL . 'assets/css/tlms-admin.css', false, TLMS_VERSION);
 
 		//Register scripts
-		wp_register_script('tlms-admin', TLMS_BASEURL . '/assets/js/tlms-admin.js', false, TLMS_VERSION);
+		wp_register_script('tlms-admin', TLMS_BASEURL . 'assets/js/tlms-admin.js', false, TLMS_VERSION);
 
 		$translations_array = array(
 			'progress_message' => __('Please wait while syncing..', 'talentlms'),
@@ -42,22 +42,22 @@ class Enqueue implements PluginService{
 
 		//Enqueue styles
 		wp_enqueue_style('tlms-admin');
-		wp_enqueue_style('tlms-datatables-css', TLMS_BASEURL . '/resources/DataTables-1.10.15/media/css/jquery.dataTables.css');
+		wp_enqueue_style('tlms-datatables-css', TLMS_BASEURL . 'resources/DataTables-1.10.15/media/css/jquery.dataTables.css');
 
 		//Enqueue scripts
 		wp_localize_script('tlms-admin', 'translations', $translations_array );
 		wp_enqueue_script('tlms-admin');
-		wp_enqueue_script('tlms-datatables-js', TLMS_BASEURL . '/resources/DataTables-1.10.15/media/js/jquery.dataTables.js');
+		wp_enqueue_script('tlms-datatables-js', TLMS_BASEURL . 'resources/DataTables-1.10.15/media/js/jquery.dataTables.js');
 	}
 
 	function tlms_enqueueFrontScripts() {
 		$this->tlms_commonLibs();
 
 		//Register styles
-		wp_register_style('tlms-front', TLMS_BASEURL . '/assets/css/talentlms.css', false, TLMS_VERSION);
+		wp_register_style('tlms-front', TLMS_BASEURL . 'assets/css/talentlms.css', false, TLMS_VERSION);
 
-		wp_enqueue_style('tlms-datatables-css', TLMS_BASEURL . '/resources/DataTables-1.10.15/media/css/jquery.dataTables.css');
-
-		wp_enqueue_script('tlms-datatables-js', TLMS_BASEURL . '/resources/DataTables-1.10.15/media/js/jquery.dataTables.js');
+		wp_enqueue_style('tlms-front');
+		wp_enqueue_style('tlms-datatables-css', TLMS_BASEURL . 'resources/DataTables-1.10.15/media/css/jquery.dataTables.css');
+		wp_enqueue_script('tlms-datatables-js', TLMS_BASEURL . 'resources/DataTables-1.10.15/media/js/jquery.dataTables.js');
 	}
 }
