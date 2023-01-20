46f0ddf34a8c53165c26c4a74d0596fedd2ef464
cpsychogyios <95210555+cpsychogyios@users.noreply.github.com>
2023-01-20 12:15:35 +0200

    Fix Ajax class, add return type to methods, introduce validation about course id, add try catch statements

--- src/Ajax.php
+++ src/Ajax.php
@@ -7,7 +7,7 @@ namespace TalentlmsIntegration;
 
 use TalentLMS_Siteinfo;
 use TalentlmsIntegration\Services\PluginService;
-use TalentlmsIntegration\Utils;
+use TalentlmsIntegration\Validations\PositiveInteger;
 
 class Ajax implements PluginService{
 
@@ -15,27 +15,34 @@ class Ajax implements PluginService{
 		add_action('wp_ajax_tlms_resynch', array($this, 'tlms_resyncCourse'));
 	}
 
-	function tlms_resyncCourse(){
+	public function tlms_resyncCourse(): void{
 		global $wpdb;
+		try{
+			$courseId = (new PositiveInteger($_POST['course_id']))->getValue();
+			$limit = TalentLMS_Siteinfo::getRateLimit();
 
-		$limit = TalentLMS_Siteinfo::getRateLimit();
+			if(!empty($courseId) && (empty($limit['remaining']) || $limit['remaining'] > 4)){ // we are gonna make at least 3 api calls, so we have to be prepared.
+				$product_ID = $wpdb->get_var("SELECT product_id FROM ".TLMS_PRODUCTS_TABLE." WHERE course_id = ".$courseId);
 
-		if(!empty($_POST['course_id']) && (empty($limit['remaining']) || $limit['remaining'] > 4)){ // we are gonna make at least 3 api calls, so we have to be prepared.
-			$product_ID = $wpdb->get_var("SELECT product_id FROM ".TLMS_PRODUCTS_TABLE." WHERE course_id = ".$_POST['course_id']);
+				if($product_ID){
+					$wpdb->query("DELETE FROM ".TLMS_PRODUCTS_TABLE." WHERE course_id = ".$courseId);
+					$wpdb->query("DELETE FROM ".TLMS_COURSES_TABLE." WHERE id = ".$courseId);
+					$wpdb->query("DELETE FROM ".WP_POSTS_TABLE." WHERE ID = ".$product_ID);
 
-			if($product_ID){
-				$wpdb->query("DELETE FROM ".TLMS_PRODUCTS_TABLE." WHERE course_id = ".$_POST['course_id']);
-				$wpdb->query("DELETE FROM ".TLMS_COURSES_TABLE." WHERE id = ".$_POST['course_id']);
-				$wpdb->query("DELETE FROM ".WP_POSTS_TABLE." WHERE ID = ".$product_ID);
-
-				Utils::tlms_getCourses(true);
-				Utils::tlms_getCategories(true);
-				Utils::tlms_addProduct($_POST['course_id'], Utils::tlms_selectCourses());
+					Utils::tlms_getCourses(true);
+					Utils::tlms_getCategories(true);
+					Utils::tlms_addProduct($courseId, Utils::tlms_selectCourses());
+				}
+				echo json_encode(array('api_limitation' => 'none'), JSON_THROW_ON_ERROR);
 			}
-			echo json_encode(array('api_limitation' => 'none'));
+			else{
+				echo json_encode(array('api_limitation' => 'Api usage resets at '.$limit['formatted_reset']), JSON_THROW_ON_ERROR);
+			}
 		}
-		else{
-			echo json_encode(array('api_limitation' => 'Api usage resets at '.$limit['formatted_reset']));
+		catch(\Exception $exception){
+			if($exception instanceof \InvalidArgumentException){
+				echo json_encode(array('error' => 'Invalid course id'), JSON_THROW_ON_ERROR);
+			}
 		}
 
 		wp_die();
