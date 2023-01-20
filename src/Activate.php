9771126fda066483073171e977c72167ac4c5029
cpsychogyios <95210555+cpsychogyios@users.noreply.github.com>
2023-01-20 09:37:24 +0200

    Remove unused methods and global vars

--- src/Activate.php
+++ src/Activate.php
@@ -3,7 +3,6 @@
  * @package talentlms-wordpress
  */
 namespace TalentlmsIntegration;
-use TalentlmsIntegration\Database;
 
 class Activate {
 
@@ -27,12 +26,9 @@ class Activate {
 
 	public static function tlms_setupWPPages() {
 		self::tlms_addCoursesPage();
-		//self::tlms_addSignupPage(); //deprecated
 	}
 
 	public static function tlms_addCoursesPage() {
-		global $wpdb;
-
 		$the_page_title = 'Courses';
 		$the_page_name = 'courses';
 
@@ -44,6 +40,7 @@ class Activate {
 		add_option("tlms_courses_page_id", '0', '', 'yes');
 
 		$the_page = get_page_by_title($the_page_title);
+
 		if (!$the_page) {
 			$_p = array();
 			$_p['post_title'] = $the_page_title;
@@ -55,44 +52,11 @@ class Activate {
 			$_p['post_category'] = array(1);
 			$the_page_id = wp_insert_post($_p);
 		} else {
-			$the_page_id = $the_page -> ID;
 			$the_page -> post_status = 'publish';
 			$the_page_id = wp_update_post($the_page);
 		}
+
 		delete_option('tlms_courses_page_id');
 		add_option('tlms_courses_page_id', $the_page_id);
-	}
-
-	public static function tlms_addSignupPage() {
-		global $wpdb;
-
-		$the_page_title = 'Signup';
-		$the_page_name = 'signup';
-
-		delete_option("tlms_signup_page_title");
-		add_option("tlms_signup_page_title", $the_page_title, '', 'yes');
-		delete_option("tlms_signup_page_name");
-		add_option("tlms_signup_page_name", $the_page_name, '', 'yes');
-		delete_option("tlms_signup_page_id");
-		add_option("tlms_signup_page_id", '1', '', 'yes');
-
-		$the_page = get_page_by_title($the_page_title);
-		if (!$the_page) {
-			$_p = array();
-			$_p['post_title'] = $the_page_title;
-			$_p['post_content'] = "[talentlms-signup]";
-			$_p['post_status'] = 'publish';
-			$_p['post_type'] = 'page';
-			$_p['comment_status'] = 'closed';
-			$_p['ping_status'] = 'closed';
-			$_p['post_category'] = array(1);
-			$the_page_id = wp_insert_post($_p);
-		} else {
-			$the_page_id = $the_page -> ID;
-			$the_page -> post_status = 'publish';
-			$the_page_id = wp_update_post($the_page);
-		}
-		delete_option('tlms_signup_page_id');
-		add_option('tlms_signup_page_id', $the_page_id);
 	}
 }
