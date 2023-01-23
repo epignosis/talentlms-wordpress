9865b9b646443f4a6e8e447533bfd98e91b2ddbe
C.Psychogyios <95210555+cpsychogyios@users.noreply.github.com>
2023-01-23 15:21:34 +0200

    #avoid saving css data into plugin folder (#21)
    * Alter cssPage method to use upload folder instead of plugin's one
    
    * Save css data into upload folder, as suggested
    
    * Introduce new template file for css page
    
    * Temporary disable css class 'fade', because it is malfunctioning
    
    * Remove extra slashes from Enqueue.php and add new css class
    
    * Add fade css class
    
    * Create separate method to enqueue custom css styles
    
    * Remove unused code and files, also change methods from protected to public
    
    * Avoid saving any tags
    
    * Add an empty line at the end
    
    * Fix hook name and the url of the recent uploaded file
    
    * Move setting upload dir, into talentlms.php and define it as global var
    
    * Use single quotes
    
    * Revert last changes, add single quotes instead of double

--- src/Enqueue.php
+++ src/Enqueue.php
@@ -5,21 +5,24 @@
 
 namespace TalentlmsIntegration;
 
+use TalentlmsIntegration\Pages\Admin;
+
 class Enqueue {
 
 	public function register(){
 		add_action('admin_enqueue_scripts', array($this, 'tlms_enqueueAdminScripts'));
+		add_action('wp_enqueue_scripts', array($this, 'tlms_enqueueStyles'));
 	}
 
-	function tlms_enqueueAdminScripts() {
+	public function tlms_enqueueAdminScripts() {
 
 		//Register styles
-		wp_register_style('tlms-admin', TLMS_BASEURL . '/assets/css/tlms-admin.css', false, TLMS_VERSION);
-		wp_register_style('tlms-widget', TLMS_BASEURL . '/assets/css/talentlms-widget.css', false, TLMS_VERSION);
+		wp_register_style('tlms-admin', TLMS_BASEURL . 'assets/css/tlms-admin.css', false, TLMS_VERSION);
+		wp_register_style('tlms-widget', TLMS_BASEURL . 'assets/css/talentlms-widget.css', false, TLMS_VERSION);
 
 		//Register scripts
-		wp_register_script('tlms-admin', TLMS_BASEURL . '/assets/js/tlms-admin.js', false, TLMS_VERSION);
-		wp_register_script('bootstrap-js', TLMS_BASEURL . '/assets/js/bootstrap.min.js', ['jquery'], false, true);
+		wp_register_script('tlms-admin', TLMS_BASEURL . 'assets/js/tlms-admin.js', false, TLMS_VERSION);
+		wp_register_script('bootstrap-js', TLMS_BASEURL . 'assets/js/bootstrap.min.js', ['jquery'], false, true);
 		wp_register_script('tlms-font-awesome', TLMS_BASEURL . '/assets/js/font-awesome.min.js', false, TLMS_VERSION);
 
 		$translations_array = array(
@@ -32,13 +35,19 @@ class Enqueue {
 		//Enqueue styles
 		wp_enqueue_style('tlms-admin');
 		wp_enqueue_style('tlms-widget');
-		wp_enqueue_style('tlms-datatables-css', TLMS_BASEURL . '/resources/DataTables-1.10.15/media/css/jquery.dataTables.css');
+		wp_enqueue_style('tlms-datatables-css', TLMS_BASEURL . 'resources/DataTables-1.10.15/media/css/jquery.dataTables.css');
 
 		//Enqueue scripts
 		wp_localize_script('tlms-admin', 'translations', $translations_array );
 		wp_enqueue_script('tlms-admin');
-		wp_enqueue_script('tlms-datatables-js', TLMS_BASEURL . '/resources/DataTables-1.10.15/media/js/jquery.dataTables.js');
+		wp_enqueue_script('tlms-datatables-js', TLMS_BASEURL . 'resources/DataTables-1.10.15/media/js/jquery.dataTables.js');
 		wp_enqueue_script('bootstrap-js');
 		wp_enqueue_script('tlms-font-awesome');
+	}
+
+
+	public function tlms_enqueueStyles() {
+		wp_register_style('tlms-custom-css', Admin::getCustomCssFilePath(), false, TLMS_VERSION);
+		wp_enqueue_style('tlms-custom-css');
 	}
 }
