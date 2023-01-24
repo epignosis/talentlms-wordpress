5c699fd7ee942b7ef11e63c23dcaea413bc649aa
cpsychogyios <95210555+cpsychogyios@users.noreply.github.com>
2023-01-24 14:54:21 +0200

    Codestyle fixes, escaping retrievable data from db

--- src/Helpers/TalentLMSApiIntegrationHelper.php
+++ src/Helpers/TalentLMSApiIntegrationHelper.php
@@ -15,16 +15,16 @@ trait TalentLMSApiIntegrationHelper
     public function enableTalentLMSLib(): void
     {
         if (empty(get_option('tlms-domain')) || empty(get_option('tlms-apikey'))) {
-            throw new TalentLMS_ApiError(__('You need to specify a TalentLMS domain and a TalentLMS API key.', 'talentlms'));
+            throw new TalentLMS_ApiError(
+                esc_html__('You need to specify a TalentLMS domain and a TalentLMS API key.', 'talentlms')
+            );
         }
 
         try {
-            TalentLMS::setDomain(get_option('tlms-domain'));
-            TalentLMS::setApiKey(get_option('tlms-apikey'));
+            TalentLMS::setDomain(esc_html(get_option('tlms-domain')));
+            TalentLMS::setApiKey(esc_html(get_option('tlms-apikey')));
         } catch (Exception $e) {
-            if ($e instanceof TalentLMS_ApiError) {
-                tlms_logError($e->getMessage());
-            }
+            tlms_logError($e->getMessage());
         }
     }
 }
