79c45cdf893d4d4c30f4d5f0d612a0c2dcfcb1b6
cpsychogyios <95210555+cpsychogyios@users.noreply.github.com>
2023-01-24 16:06:05 +0200

    Codestyle fixes, sanitization & escaping

--- src/Pages/Admin.php
+++ src/Pages/Admin.php
@@ -25,50 +25,80 @@ class Admin implements PluginService
         $this->enableTalentLMSLib();
     }
 
-    public function tlms_registerAdministrationPages()
+    public function tlms_registerAdministrationPages(): void
     {
-        add_menu_page(__('TalentLMS', 'talentlms'), __('TalentLMS', 'talentlms'), 'manage_options', 'talentlms', array($this, 'tlms_adminPanel'));
-        add_submenu_page('talentlms', __('Dashboard', 'talentlms'), __('Dashboard', 'talentlms'), 'manage_options', 'talentlms', array($this, 'tlms_adminPanel'));
-        add_submenu_page('talentlms', __('Setup', 'talentlms'), __('Setup', 'talentlms'), 'manage_options', 'talentlms-setup', array($this, 'tlms_setupPage'));
-        add_submenu_page('talentlms', __('Integrations', 'talentlms'), __('Integrations', 'talentlms'), 'manage_options', 'talentlms-integrations', array($this,
-                                                                                                                                                          'tlms_integrationsPage'));
-        add_submenu_page('talentlms', __('CSS', 'talentlms'), __('CSS', 'talentlms'), 'manage_options', 'talentlms-css', array($this, 'tlms_cssPage'));
+        add_menu_page(
+			esc_html_e('TalentLMS', 'talentlms'),
+			esc_html_e('TalentLMS', 'talentlms'),
+			'manage_options', 'talentlms', array($this, 'tlms_adminPanel')
+		);
+        add_submenu_page(
+			'talentlms',
+			esc_html_e('Dashboard', 'talentlms'),
+			esc_html_e('Dashboard', 'talentlms'),
+			'manage_options', 'talentlms', array($this, 'tlms_adminPanel'));
+        add_submenu_page(
+			'talentlms',
+			esc_html_e('Setup', 'talentlms'),
+			esc_html_e('Setup', 'talentlms'),
+			'manage_options', 'talentlms-setup', array($this, 'tlms_setupPage'));
+        add_submenu_page(
+			'talentlms',
+			esc_html_e('Integrations', 'talentlms'),
+			esc_html_e('Integrations', 'talentlms'),
+			'manage_options', 'talentlms-integrations', array($this,'tlms_integrationsPage'));
+        add_submenu_page(
+			'talentlms',
+			esc_html_e('CSS', 'talentlms'),
+			esc_html_e('CSS', 'talentlms'),
+			'manage_options', 'talentlms-css', array($this, 'tlms_cssPage'));
     }
 
-    function tlms_adminPanel()
+    public function tlms_adminPanel(): void
     {
         require_once TLMS_BASEPATH.'/templates/dashboard.php';
     }
 
-    public function tlms_setupPage()
+    public function tlms_setupPage(): void
     {
-
-        $action_status = $action_message = $enroll_user_validation = $automatically_complete_orders = $api_validation = $domain_validation = '';
+        $action_status = $action_message = $api_validation = $domain_validation = '';
         if (isset($_POST['action']) && $_POST['action'] == 'tlms-setup') {
-            if ($_POST['tlms-domain'] && $_POST['tlms-apikey'] && $_POST['tlms-enroll-user-to-courses']) {
+            if (
+				isset($_POST['tlms-domain'], $_POST['tlms-apikey'], $_POST['tlms-enroll-user-to-courses'])
+			) {
                 // we accept the domain only, without the protocol
-                if (strtolower(substr($_POST['tlms-domain'], 0, 4)) === 'http') {
-                    $action_status = "error";
+                if (
+					stripos(strtolower($_POST['tlms-domain']), 'http') === 0
+					|| stripos(strtolower($_POST['tlms-domain']), 'https') === 0
+				) {
+                    $action_status = 'error';
                     $domain_validation = 'form-invalid';
                     $action_message = __('Invalid TalentLMS Domain', 'talentlms').'<br />';
-                } elseif (strlen($_POST['tlms-apikey']) != 30) { // TalentLMS API key is exactly 30 characters
-                    $action_status = "error";
+                } elseif (strlen($_POST['tlms-apikey']) !== 30) { // TalentLMS API key is exactly 30 characters
+                    $action_status = 'error';
                     $api_validation = 'form-invalid';
                     $action_message = __('Invalid TalentLMS API key', 'talentlms').'<br />';
                 } else {
-                    update_option('tlms-domain', esc_sql($_POST['tlms-domain']));
-                    update_option('tlms-apikey', esc_sql($_POST['tlms-apikey']));
-                    update_option('tlms-enroll-user-to-courses', esc_sql($_POST['tlms-enroll-user-to-courses']));
+                    update_option('tlms-domain', esc_url_raw($_POST['tlms-domain']));
+                    update_option('tlms-apikey', sanitize_text_field($_POST['tlms-apikey']));
+                    update_option(
+						'tlms-enroll-user-to-courses',
+						sanitize_text_field($_POST['tlms-enroll-user-to-courses'])
+					);
 
                     if (isset($_POST['tlms-automtically-complete-orders'])) {
-                        update_option('tlms-automtically-complete-orders', esc_sql($_POST['tlms-automtically-complete-orders']));
+                        update_option(
+							'tlms-automtically-complete-orders',
+							sanitize_text_field($_POST['tlms-automtically-complete-orders']
+							)
+						);
                     }
 
                     $action_status = 'updated';
                     $action_message = __('Details edited successfully', 'talentlms');
                 }
             } else {
-                $action_status = "error";
+                $action_status = 'error';
 
                 if (!$_POST['tlms-domain']) {
                     $domain_validation = 'form-invalid';
@@ -78,7 +108,7 @@ class Admin implements PluginService
 
                 if (!$_POST['tlms-apikey']) {
                     $api_validation = 'form-invalid';
-                    $action_message .= __('TalentLMS API key required', 'talentlms').'<br />';
+                    $action_message = __('TalentLMS API key required', 'talentlms').'<br />';
                     update_option('tlms-apikey', '');
                 }
             }
@@ -87,9 +117,8 @@ class Admin implements PluginService
         require_once TLMS_BASEPATH.'/templates/setup.php';
     }
 
-    public function tlms_integrationsPage()
+    public function tlms_integrationsPage(): void
     {
-
         $courses = Utils::tlms_selectCourses();
 
         if (isset($_POST['tlms_products']) && $_POST['tlms_products']) {
@@ -123,7 +152,7 @@ class Admin implements PluginService
         $upload_dir = wp_upload_dir();
         $dir = trailingslashit($upload_dir['basedir']).TLMS_UPLOAD_DIR;
 
-        $customCssFileName = $dir."/css/talentlms-style.css";
+        $customCssFileName = $dir.'/css/talentlms-style.css';
         $customCssFileContent = null;
 
         if ($_POST['action'] == 'edit-css') {
@@ -133,8 +162,8 @@ class Admin implements PluginService
             }
 
             // Create a subfolder in my new folder if not exist
-            if (!$wp_filesystem->is_dir($dir."/css")) {
-                $wp_filesystem->mkdir($dir."/css");
+            if (!$wp_filesystem->is_dir($dir.'/css')) {
+                $wp_filesystem->mkdir($dir.'/css');
             }
 
             if ($wp_filesystem->exists($customCssFileName)) {
--- templates/integrations.php
+++ templates/integrations.php
@@ -1,9 +1,9 @@
 <div class="wrap">
 
     <?php if (get_option('tlms-woocommerce-active')) : ?>
-        <h1><?php _e('Integrations', 'talentlms'); ?></h1>
+        <h1><?php esc_html_e('Integrations', 'talentlms'); ?></h1>
 
-        <div id='action-message' class='<?php echo (isset($action_status))? esc_html($action_status) : ''; ?> fade'>
+        <div id='action-message' class='<?php echo (isset($action_status))? esc_attr($action_status) : ''; ?> fade'>
             <p><?php echo (isset($action_message)) ? esc_html($action_message) : '' ?></p>
         </div>
 
@@ -11,58 +11,60 @@
         <h2>WooCommerce</h2>
 
         <!-- form to post to get new courses -->
-        <form action="<?php echo admin_url('admin.php?page=talentlms-integrations'); ?>" method="POST">
+        <form action="<?php echo esc_url(admin_url('admin.php?page=talentlms-integrations')); ?>" method="POST">
             <input type="hidden" name="action" value="tlms-fetch-courses">
             <input type="submit" class="btn button-primary" value="<?php _e('Refresh course list', 'talentlms'); ?>">
         </form>
 
         <br />
 
-        <form action="<?php echo admin_url('admin.php?page=talentlms-integrations'); ?>" method="POST" name="form">
+        <form action="<?php echo esc_url(admin_url('admin.php?page=talentlms-integrations')); ?>" method="POST" name="form">
 
             <table id="tlms-integrations-table" class="wp-list-table widefat fixed striped" cellspacing="0">
                 <thead>
                 <tr>
                     <th class="manage-column column-title" scope="col" width="150">
-                        <?php _e('Course', 'talentlms'); ?>
+                        <?php esc_html_e('Course', 'talentlms'); ?>
                     </th>
                     <th class="manage-column check-column" scope="col" style="text-align: center; padding: 10px 0;">
-                        <a id="tlms-integrate-all" href="javascript:void(0);"><?php _e('Select All', 'talentlms');?></a>
+                        <a id="tlms-integrate-all" href="javascript:void(0);"><?php esc_html_e('Select All', 'talentlms');?></a>
                     </th>
                 </tr>
                 </thead>
                 <tbody>
-                <?php foreach ($courses as $course) : ?>
-                    <?php if (!$course->hide_catalog && $course->status == 'active') : ?>
-                        <tr>
-                            <td class="column-title">
-                                <?php echo esc_html($course->name)?>
+				<?php if(!empty($courses)): ?>
+					<?php foreach ($courses as $course) : ?>
+						<?php if (!$course->hide_catalog && $course->status == 'active') : ?>
+							<tr>
+								<td class="column-title">
+									<?php echo esc_html($course->name)?>
 
-                                <div class="row-actions">
-                                <span class="inline hide-if-no-js">
-                                    <a class="tlms-reset-course" data-course-id="<?php echo esc_attr($course->id); ?>" href="#"><?php _e('Re-Sync', 'talentlms'); ?></a>
-                                </span>
-                                </div>
+									<div class="row-actions">
+										<span class="inline hide-if-no-js">
+											<a class="tlms-reset-course" data-course-id="<?php echo esc_attr($course->id); ?>" href="#"><?php esc_html_e('Re-Sync', 'talentlms'); ?></a>
+										</span>
+									</div>
 
-                            </td>
-                            <td class="check-column" style="text-align: center">
-                                <input type='checkbox' class="tlms-products" name="tlms_products[]" autocomplete="off" value="<?php echo esc_attr($course->id); ?>" <?php echo (\TalentlmsIntegration\Utils::tlms_productExists($course->id)) ? 'checked' : ''; ?>/>
-                            </td>
-                        </tr>
-                    <?php endif;?>
-                <?php endforeach;?>
+								</td>
+								<td class="check-column" style="text-align: center">
+									<input type='checkbox' class="tlms-products" name="tlms_products[]" autocomplete="off" value="<?php echo $course->id; ?>" <?php echo (\TalentlmsIntegration\Utils::tlms_productExists($course->id)) ? 'checked' : ''; ?>/>
+								</td>
+							</tr>
+						<?php endif;?>
+					<?php endforeach;?>
+				<?php endif;?>
                 </tbody>
             </table>
 
             <p class="submit">
-                <input class="button button-primary" type="submit" value="<?php _e('Integrate', 'talentlms')?>" title="<?php _e('add selected courses as woocommerce products', 'talentlms')?>">
+                <input class="button button-primary" type="submit" value="<?php _e('Integrate', 'talentlms')?>" title="<?php esc_html_e('add selected courses as woocommerce products', 'talentlms')?>">
             </p>
 
         </form>
 
     <?php else : ?>
         <div id='action-message' class='error fade'>
-            <p><?php _e('WooCommerce is not activated please activate the plugin to use the integrations page', 'talentlms'); ?></p>
+            <p><?php esc_html_e('WooCommerce is not activated please activate the plugin to use the integrations page', 'talentlms'); ?></p>
         </div>
 
 
