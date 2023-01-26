7b840281c830261f5452aaa98ca7a834bafc4df5
Eleni Polikrati <104966119+epolikrati@users.noreply.github.com>
2023-01-26 13:38:49 +0200

    Iclude DataTables library in plugin core (#28)

--- src/Enqueue.php
+++ src/Enqueue.php
@@ -16,14 +16,23 @@ class Enqueue implements PluginService{
 	}
 
 	private function tlms_commonLibs(){
+		//Register styles
 		wp_register_style('tlms-widget', TLMS_BASEURL . 'assets/css/talentlms-widget.css', false, TLMS_VERSION);
+		wp_register_style('tlms-datatables-css', TLMS_BASEURL . 'assets/css/jquery.dataTables.min.css');
 
+		//Register scripts
 		wp_register_script('bootstrap-js', TLMS_BASEURL . 'assets/js/bootstrap.min.js', ['jquery'], false, true);
 		wp_register_script('tlms-font-awesome', TLMS_BASEURL . 'assets/js/font-awesome.min.js', false, TLMS_VERSION);
+		wp_register_script('tlms-datatables-js', TLMS_BASEURL . 'assets/js/jquery.dataTables.min.js', ['jquery']);
 
+		//Enqueue styles
 		wp_enqueue_style('tlms-widget');
+		wp_enqueue_style('tlms-datatables-css');
+
+		//Enqueue scripts
 		wp_enqueue_script('bootstrap-js');
 		wp_enqueue_script('tlms-font-awesome');
+		wp_enqueue_script('tlms-datatables-js');
 	}
 	function tlms_enqueueAdminScripts() {
 		$this->tlms_commonLibs();
@@ -43,12 +52,10 @@ class Enqueue implements PluginService{
 
 		//Enqueue styles
 		wp_enqueue_style('tlms-admin');
-		wp_enqueue_style('tlms-datatables-css', TLMS_BASEURL . 'resources/DataTables-1.10.15/media/css/jquery.dataTables.css');
 
 		//Enqueue scripts
 		wp_localize_script('tlms-admin', 'translations', $translations_array );
 		wp_enqueue_script('tlms-admin');
-		wp_enqueue_script('tlms-datatables-js', TLMS_BASEURL . 'resources/DataTables-1.10.15/media/js/jquery.dataTables.js');
 	}
 
 	function tlms_enqueueFrontScripts() {
@@ -58,8 +65,6 @@ class Enqueue implements PluginService{
 		wp_register_style('tlms-front', TLMS_BASEURL . 'assets/css/talentlms.css', false, TLMS_VERSION);
 
 		wp_enqueue_style('tlms-front');
-		wp_enqueue_style('tlms-datatables-css', TLMS_BASEURL . 'resources/DataTables-1.10.15/media/css/jquery.dataTables.css');
-		wp_enqueue_script('tlms-datatables-js', TLMS_BASEURL . 'resources/DataTables-1.10.15/media/js/jquery.dataTables.js');
 	}
 
 
