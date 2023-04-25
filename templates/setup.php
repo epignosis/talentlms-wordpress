c77e215563ea5c8768c0b0dc5d78fcb849ec35ce
DimChristodoulou <dchristodoulou.di@gmail.com>
2023-04-25 11:32:38 +0300

    Convert parity echo to if statement

--- templates/setup.php
+++ templates/setup.php
@@ -31,8 +31,8 @@
                 </th>
                 <td class="form-field form-required <?php echo esc_attr($enroll_user_validation); ?>">
                     <select name="tlms-enroll-user-to-courses">
-                        <option value="submission" <?php echo (get_option('tlms-enroll-user-to-courses') == 'submission')? 'selected="Selected"': ''; ?> ><?php esc_html_e("Upon order submission", 'talentlms'); ?></option>
-                        <option value="completion" <?php echo (get_option('tlms-enroll-user-to-courses') == 'completion')? 'selected="Selected"': ''; ?> ><?php esc_html_e("Upon order completion", 'talentlms'); ?></option>
+                        <option value="submission" <?php if(get_option('tlms-enroll-user-to-courses') == 'submission'){ echo 'selected="Selected"';}else{ echo '';} ?> ><?php esc_html_e("Upon order submission", 'talentlms'); ?></option>
+                        <option value="completion" <?php if(get_option('tlms-enroll-user-to-courses') == 'completion'){ echo 'selected="Selected"';}else{ echo '';} ?> ><?php esc_html_e("Upon order completion", 'talentlms'); ?></option>
                     </select>
                 </td>
             </tr>
